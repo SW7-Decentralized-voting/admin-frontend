93f15b67ffec8385ae0f78291fbd0bf3822cf423
Mafusn <Magnus.peetz.holt@gmail.com>
2024-10-11 09:53:14 +0200

    Init cloning of voter FE

---
+++ eslint.config.js
@@ -0,0 +1,60 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import jest from 'eslint-plugin-jest';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+
+export default [
+  { ignores: ['dist'] },
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    settings: { react: { version: '18.3' } },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+      'react/jsx-no-target-blank': 'off',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single'],
+      'no-unused-vars': 'error',
+      'no-undef': 'error',
+      'no-console': 'warn',
+      'no-extra-semi': 'error',
+      'no-unreachable': 'error',
+      'no-constant-condition': 'error',
+      'no-empty': 'error',
+    },
+  },
+  {
+    files: ['__test__/*', '**/*.test.js'],
+    plugins: { jest },
+    rules: {
+      ...jest.configs.recommended.rules,
+    },
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+      }
+    }
+  },
+  pluginJs.configs.recommended,
+];
---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
+import VotingPage from './VotingPage';
+import HashVerificationPage from './HashVerificationPage';
+import './App.css';
+
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/voting" element={<VotingPage />} />
+        <Route path="/verify" element={<HashVerificationPage />} />
+      </Routes>
+    </>
+  );
+}
+
+
+export default App;
